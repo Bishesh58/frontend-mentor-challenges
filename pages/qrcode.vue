7a5fd2cc1a890db3ce09fec18b8aa1c0d5d24d79
Bishesh <bishesh.sunam@gmail.com>
2023-10-18 18:45:52 +1300

    refactor: change qr code font size

--- pages/qrcode.vue
+++ pages/qrcode.vue
@@ -2,9 +2,9 @@
   <div class="min-h-screen flex justify-center items-center bg-[#D6E2F0]">
     <div class="w-[320px] h-[500px] bg-[#FFFFFF] p-4 rounded-xl">
       <img src="../public/img/qr-code.png" alt="qrcode" class="rounded-md" />
-      <h1 class="font-bold text-[#1F3251] text-lg py-4">
+      <p class="font-bold text-[#1F3251] text-xl py-4 px-4">
         Improve your front-end skills by building projects
-      </h1>
+      </p>
       <p class="text-[#7B879D] text-center font-Raleway">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
