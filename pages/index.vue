41ca1ef582d625c2fea9d5d120999e58f8a89ae0
Bishesh58 <bishesh.sunam@gmail.com>
2023-10-19 15:51:41 +1300

    feat: add order card on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="h-screen flex flex-col justify-between">
-    <div class="p-4 flex flex-col justify-center items-center flex-1">
+    <div class="p-4 flex flex-col justify-start items-center flex-1">
       <p class="max-md:text-2xl text-4xl font-semibold font-Raleway">
         <span class="text-[#3E54A3]">Frontend Mentor</span> Challenge Projects
       </p>
-      <div class="w-full h-full flex justify-center flex-wrap p-8">
+      <div class="flex justify-center flex-wrap p-8 gap-6">
         <NuxtLink to="/qrcode"
           ><div class="w-[300px] h-[300px] border rounded-md flex flex-col">
             <img
@@ -14,6 +14,18 @@
             />
             <div class="p-4 text-center">
               <p>QR Code Component</p>
+            </div>
+          </div>
+        </NuxtLink>
+        <NuxtLink to="/order-card" class="w-fit"
+          ><div class="w-[300px] h-[300px] border rounded-md flex flex-col">
+            <img
+              src="../public/img/order_preview.png"
+              alt="qr_preview"
+              class="flex-1 w-full h-[60%] object-cover object-center"
+            />
+            <div class="p-4 text-center">
+              <p>Order Summary Component</p>
             </div>
           </div>
         </NuxtLink>
