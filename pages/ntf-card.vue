d163d6f4341b05856c3637f231e61fc963662931
Bishesh <bishesh.sunam@gmail.com>
2023-10-21 13:10:43 +1300

    feat: add hover state on ntf card component

--- pages/ntf-card.vue
+++ pages/ntf-card.vue
@@ -3,8 +3,23 @@
     class="min-h-screen flex justify-center items-center bg-[#0D1A2D] font-Outfit font-[300]"
   >
     <div class="w-[320px] bg-[#15273F] p-4 rounded-xl">
-      <img src="/img/image-equilibrium.jpg" alt="qrcode" class="rounded-md" />
-      <p class="text-[#f3f3f3] text-xl py-4">Equilibrium #3429</p>
+      <div class="hover:cursor-pointer rounded-md relative group">
+        <img src="/img/image-equilibrium.jpg" alt="qrcode" class="rounded-md" />
+        <div
+          class="bg-[#19FFFF] opacity-50 absolute top-0 bottom-0 left-0 right-0 hidden group-hover:flex justify-center items-center rounded-md"
+        ></div>
+        <div
+          class="absolute top-0 bottom-0 left-0 right-0 hidden justify-center items-center group-hover:flex"
+        >
+          <img src="/svg/icon-view.svg" alt="" />
+        </div>
+      </div>
+
+      <p
+        class="text-[#f3f3f3] text-xl py-4 hover:text-[#19FFFF] hover:cursor-pointer"
+      >
+        Equilibrium #3429
+      </p>
       <p class="text-[#97AECD] text-left">
         Our Equilibrium collection promotes balance and calm.
       </p>
@@ -26,7 +41,10 @@
           class="h-8 w-8 rounded-full"
         />
         <p class="text-[#97AECD]">
-          Creation of <span class="text-[#f3f3f3]">Jules Wyvern</span>
+          Creation of
+          <span class="text-[#f3f3f3] hover:text-[#19FFFF] hover:cursor-pointer"
+            >Jules Wyvern</span
+          >
         </p>
       </div>
     </div>
