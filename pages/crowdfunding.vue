e6221f3ba1f1a1360bd7ff490a45123ed72ab9e6
Bishesh <bishesh.sunam@gmail.com>
2023-11-03 19:36:10 +1300

    feat: add crowdfunding page modals

--- pages/crowdfunding.vue
+++ pages/crowdfunding.vue
@@ -43,6 +43,7 @@
           <div class="flex justify-between items-center max-md:gap-4 pt-8 pb-4">
             <button
               class="rounded-full max-md:flex-1 px-4 py-2.5 bg-[#3CB4AC] hover:bg-[#147B74] text-white text-sm"
+              @click="isBackModal = true"
             >
               Back this project
             </button>
@@ -175,19 +176,6 @@
               </button>
             </div>
           </div>
-
-          <!-- Selected pledge start -->
-          <div>Enter your pledge $25 Continue</div>
-
-          <!-- Selected pledge start -->
-          <div>Enter your pledge $75 Continue</div>
-          <!-- Selected pledge end -->
-
-          <!-- Selected pledge  start -->
-          <div>Enter your pledge $200 Continue</div>
-          <!-- Selected pledge end -->
-
-          <!-- Selection modal end -->
         </div>
       </main>
     </div>
@@ -217,7 +205,7 @@
     <!-- Success modal start -->
     <div
       v-if="isModalSuccesss"
-      class="absolute -top-40 left-0 right-0 bottom-0 z-50 bg-black/50 text-sm flex justify-center items-center"
+      class="absolute -top-40 left-0 right-0 bottom-0 z-50 bg-black/50 text-sm flex justify-center items-center m-2"
     >
       <div
         class="p-10 m-6 max-w-md rounded-md bg-white text-center flex flex-col justify-center items-center gap-4 shadow-md"
@@ -235,52 +223,119 @@
         </p>
         <button
           class="rounded-full px-8 py-2 bg-[#3CB4AC] hover:bg-[#147B74] text-white w-fit"
-          @click="isModalSuccesss = false"
+          @click="resetModal"
         >
           got it!
         </button>
       </div>
     </div>
     <!-- Success modal end -->
+    <!-- pledge modal start -->
     <div
-      class="absolute -top-40 left-0 right-0 bottom-0 z-50 bg-black/50 text-sm flex justify-center items-center"
+      v-if="isBackModal"
+      class="absolute -top-40 left-0 right-0 bottom-0 z-40 bg-black/50 text-sm flex justify-center items-center"
     >
       <div
-        class="p-10 m-6 max-w-2xl rounded-md bg-white flex flex-col justify-center items-center gap-4 shadow-md"
+        class="p-6 md:p-10 m-6 max-w-2xl rounded-md bg-white flex flex-col justify-center items-center gap-4 shadow-md"
       >
         <div class="flex justify-between w-full">
           <h2 class="font-semibold text-xl">Back this project</h2>
-          <Icon name="mdi:close" size="16px" />
+          <Icon
+            name="mdi:close"
+            size="20px"
+            @click="closePledgeModal"
+            class="hover:cursor-pointer hover:font-bold hover:bg-gray-300"
+          />
         </div>
         <p class="text-sm text-gray-400 w-full text-left">
           Want to support us in bringing Mastercraft Bamboo Monitor Riser out in
           the world?
         </p>
-        <div class="border rounded-md">
+        <!-- p1 -->
+        <div
+          class="border rounded-md hover:shadow-md hover:cursor-pointer"
+          :class="{ 'border-[#3CB4AC] border-2': isSelected[0] }"
+          @click="togglePledge(0)"
+        >
           <div class="p-6">
             <div class="flex items-center relative">
               <div class="self-start">
                 <div
                   class="h-6 w-6 border rounded-full flex justify-center items-center"
                 >
-                  <div class="h-3.5 w-3.5 bg-[#3CB4AC] rounded-full"></div>
+                  <div
+                    v-if="isSelected[0]"
+                    class="h-3.5 w-3.5 bg-[#3CB4AC] rounded-full"
+                  ></div>
+                </div>
+              </div>
+              <div class="text-left px-6">
+                <h2 class="font-semibold text-sm">
+                  Pledge with no reward
+                  <span class="text-[#3CB4AC] text-sm md:px-2 block md:inline"
+                    >Pledge $25 or more</span
+                  >
+                </h2>
+                <p
+                  class="text-sm text-gray-400 leading-relaxed py-4 -ml-12 mb-6 md:mb-0 md:-ml-0"
+                >
+                  Choose to support us without a reward if you simply believe in
+                  our project. As a backer, you will be signed up to receive
+                  product updates via email.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+          <div
+            v-if="isSelected[0]"
+            @click.stop
+            class="flex justify-center md:justify-end items-center p-6"
+          >
+            <button
+              class="rounded-full px-4 py-2 bg-[#3CB4AC] hover:bg-[#147B74] text-white text-sm"
+              @click="isModalSuccesss = true"
+            >
+              continue
+            </button>
+          </div>
+        </div>
+        <!-- p2 -->
+        <div
+          class="border rounded-md hover:shadow-md hover:cursor-pointer"
+          :class="{ 'border-[#3CB4AC] border-2': isSelected[1] }"
+          @click="togglePledge(1)"
+        >
+          <div class="p-6">
+            <div class="flex items-center relative">
+              <div class="self-start">
+                <div
+                  class="h-6 w-6 border rounded-full flex justify-center items-center"
+                >
+                  <div
+                    v-if="isSelected[1]"
+                    class="h-3.5 w-3.5 bg-[#3CB4AC] rounded-full"
+                  ></div>
                 </div>
               </div>
               <div class="text-left px-6">
                 <h2 class="font-semibold text-sm">
                   Bamboo Stand
-                  <span class="text-[#3CB4AC] text-sm px-2"
+                  <span class="text-[#3CB4AC] text-sm md:px-2 block md:inline"
                     >Pledge $25 or more</span
                   >
                 </h2>
-                <p class="text-sm text-gray-400 leading-relaxed py-4">
+                <p
+                  class="text-sm text-gray-400 leading-relaxed py-4 -ml-12 mb-6 md:mb-0 md:-ml-0"
+                >
                   You get an ergonomic stand made of natural bamboo. You've
                   helped us launch our promotional campaign, and you’ll be added
                   to a special Backer member list.
                 </p>
               </div>
               <p
-                class="absolute max-md:left-0 max-md:bottom-0 md:top-0 md:right-0 font-bold"
+                class="absolute max-md:text-lg max-md:left-0 max-md:bottom-0 md:top-0 md:right-0 font-bold"
               >
                 101
                 <span class="text-xs text-gray-400 font-normal px-1">left</span>
@@ -289,27 +344,148 @@
           </div>
 
           <hr />
-          <div class="flex justify-between p-6">
-            <p class="text-gray-400">Enter your pledge</p>
-            <div class="flex justify-between gap-4">
-              <div class="px-4 py-2 rounded-full border w-18">
+          <div
+            v-if="isSelected[1]"
+            @click.stop
+            class="flex flex-col md:flex-row justify-between p-6"
+          >
+            <p class="text-gray-400 max-md:text-center max-md:pb-4">
+              Enter your pledge
+            </p>
+            <div class="flex max-md:justify-center justify-between gap-4">
+              <div
+                class="px-4 py-2 rounded-full border w-auto hover:border-[#3CB4AC]"
+              >
                 <span class="px-1 text-gray-400">$</span>
                 <input
-                  type="text"
+                  type="number"
+                  min="25"
                   placeholder="25"
-                  class="inline-block w-6 outline-none placeholder:text-black font-bold"
+                  class="inline-block w-[50px] outline-none placeholder:text-black font-bold"
                 />
               </div>
               <button
                 class="rounded-full px-4 py-2 bg-[#3CB4AC] hover:bg-[#147B74] text-white text-sm"
+                @click="isModalSuccesss = true"
               >
                 continue
               </button>
             </div>
           </div>
         </div>
+        <!-- p3 -->
+        <div
+          class="border rounded-md hover:shadow-md hover:cursor-pointer"
+          :class="{ 'border-[#3CB4AC] border-2': isSelected[2] }"
+          @click="togglePledge(2)"
+        >
+          <div class="p-6">
+            <div class="flex items-center relative">
+              <div class="self-start">
+                <div
+                  class="h-6 w-6 border rounded-full flex justify-center items-center"
+                >
+                  <div
+                    v-if="isSelected[2]"
+                    class="h-3.5 w-3.5 bg-[#3CB4AC] rounded-full"
+                  ></div>
+                </div>
+              </div>
+              <div class="text-left px-6">
+                <h2 class="font-semibold text-sm">
+                  Black Edition Stand
+                  <span class="text-[#3CB4AC] text-sm md:px-2 block md:inline"
+                    >Pledge $75 or more</span
+                  >
+                </h2>
+                <p
+                  class="text-sm text-gray-400 leading-relaxed py-4 -ml-12 mb-6 md:mb-0 md:-ml-0"
+                >
+                  You get a Black Special Edition computer stand and a personal
+                  thank you. You’ll be added to our Backer member list. Shipping
+                  is included.
+                </p>
+              </div>
+              <p
+                class="absolute max-md:text-lg max-md:left-0 max-md:bottom-0 md:top-0 md:right-0 font-bold"
+              >
+                64
+                <span class="text-xs text-gray-400 font-normal px-1">left</span>
+              </p>
+            </div>
+          </div>
+
+          <hr />
+          <div
+            v-if="isSelected[2]"
+            @click.stop
+            class="flex flex-col md:flex-row justify-between p-6"
+          >
+            <p class="text-gray-400 max-md:text-center max-md:pb-4">
+              Enter your pledge
+            </p>
+            <div class="flex max-md:justify-center justify-between gap-4">
+              <div
+                class="px-4 py-2 rounded-full border w-auto hover:border-[#3CB4AC]"
+              >
+                <span class="px-1 text-gray-400">$</span>
+                <input
+                  type="number"
+                  min="75"
+                  placeholder="75"
+                  class="inline-block w-[50px] outline-none placeholder:text-black font-bold"
+                />
+              </div>
+              <button
+                class="rounded-full px-4 py-2 bg-[#3CB4AC] hover:bg-[#147B74] text-white text-sm"
+                @click="isModalSuccesss = true"
+              >
+                continue
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- p4 -->
+        <div class="border rounded-md opacity-50 hover:cursor-not-allowed">
+          <div class="p-6">
+            <div class="flex items-center relative">
+              <div class="self-start">
+                <div
+                  class="h-6 w-6 border rounded-full flex justify-center items-center"
+                >
+                  <div
+                    v-if="isSelected[3]"
+                    class="h-3.5 w-3.5 bg-[#3CB4AC] rounded-full"
+                  ></div>
+                </div>
+              </div>
+              <div class="text-left px-6">
+                <h2 class="font-semibold text-sm">
+                  Mahogany Special Edition
+                  <span class="text-[#3CB4AC] text-sm md:px-2 block md:inline"
+                    >Pledge $200 or more</span
+                  >
+                </h2>
+                <p
+                  class="text-sm text-gray-400 leading-relaxed py-4 -ml-12 mb-6 md:mb-0 md:-ml-0"
+                >
+                  You get two Special Edition Mahogany stands, a Backer T-Shirt,
+                  and a personal thank you. You’ll be added to our Backer member
+                  list. Shipping is included.
+                </p>
+              </div>
+              <p
+                class="absolute max-md:text-lg max-md:left-0 max-md:bottom-0 md:top-0 md:right-0 font-bold"
+              >
+                0
+                <span class="text-xs text-gray-400 font-normal px-1">left</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    <!-- pledge modal end -->
   </div>
 </template>
 
@@ -317,9 +493,24 @@
 const isBookmarked = ref(false);
 const isMobileMenu = ref(false);
 const isModalSuccesss = ref(false);
+const isBackModal = ref(false);
+const isSelected = ref([false, false, false, false]);
 
 const toggleBookmark = () => {
   isBookmarked.value = !isBookmarked.value;
+};
+
+const togglePledge = (index) => {
+  isSelected.value = isSelected.value.map((value, i) => i === index);
+};
+const closePledgeModal = () => {
+  isSelected.value = [false, false, false, false];
+  isBackModal.value = false;
+};
+
+const resetModal = () => {
+  isModalSuccesss.value = false;
+  closePledgeModal();
 };
 </script>
 
