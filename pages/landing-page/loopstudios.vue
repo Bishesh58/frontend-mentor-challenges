246b868a8cafc06cc751cd5e12e894c6cfa7fe27
Bishesh <bishesh.sunam@gmail.com>
2023-11-04 21:18:26 +1300

    feat: add landing page - loopstudios markup

---
+++ pages/landing-page/loopstudios.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="font-Epilogue">
+    <div
+      class="h-[600px] bg-[url('/img/image-hero.jpg')] bg-no-repeat bg-center bg-cover"
+    >
+      <div class="max-w-7xl mx-auto text-white">
+        <header class="flex justify-between items-center">
+          <img src="/svg/logo-loopstudios.svg" alt="" />
+          <nav class="flex space-x-4">
+            <a href="#">About</a>
+            <a href="#">Careers</a>
+            <a href="#">Events</a>
+            <a href="#">Products</a>
+            <a href="#">Supports</a>
+          </nav>
+        </header>
+        <h1
+          class="uppercase border border-white w-fit text-7xl font-thin tracking-widest"
+        >
+          Immersive experiences that deliver
+        </h1>
+      </div>
+    </div>
+    <div>
+      <section>
+        <img src="/img/" alt="" />
+        <div>
+          <h1>The leader in interactive VR</h1>
+          <p>
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h1>Our creations</h1>
+          <button>see all</button>
+        </div>
+        <div>
+          <!-- grid -->
+          <pre>
+                Deep earth
+                Night arcade
+                Soccer team VR
+                The grid
+                From up above VR
+                Pocket borealis
+                The curiosity
+                Make it fisheye
+            </pre
+          >
+        </div>
+      </section>
+    </div>
+    <div>
+      <!-- footer -->
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
