5947f61bf182b8d72b194744145a067e790a2322
Bishesh58 <bishesh.sunam@gmail.com>
2023-11-27 16:23:28 +1300

    feat: add interactive comment component layout

---
+++ components/CommentItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps(["comment"]);
+</script>
+
+<template>
+  <div>
+    <div
+      class="w-full md:w-[512px] px-4 border-l text-black md:px-6 rounded-md text-xs sm:text-sm md:text-base"
+    >
+      <div class="flex justify-between">
+        <div class="flex-grow flex items-center gap-2 -ml-12">
+          <img :src="comment.profileImg" alt="" class="h-10 w-10" />
+          <p>{{ comment.author }}</p>
+          <p>{{ comment.createdAt }}</p>
+        </div>
+        <button><Icon name="mdi:reply" size="16px" /> reply</button>
+      </div>
+      <p>
+        {{ comment.text }}
+      </p>
+
+      <!-- Render replies using recursion -->
+      <CommentItem
+        v-for="reply in comment.replies"
+        :key="reply.id"
+        :comment="reply"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/interactive-comments.vue
@@ -0,0 +1,85 @@
+<template>
+  <main
+    class="min-h-screen max-w-7xl mx-auto sm:p-6 md:p-8 flex flex-col items-center bg-slate-100"
+  >
+    <div class="py-8">
+      <CommentItem
+        v-for="comment in comments"
+        :key="comment.id"
+        :comment="comment"
+      />
+    </div>
+    <div class="flex items-start gap-4">
+      <div>
+        <img src="/img/user/angela.webp" alt="" class="h-10 w-10" />
+      </div>
+      <div
+        ref="commentInput"
+        contenteditable="true"
+        @input="handleInput"
+        class="h-20 w-96 border-2 border-slate-200 rounded-md outline-none p-2 bg-inherit text-gray-600 overflow-y-auto"
+        placeholder="Write.."
+      ></div>
+      <button
+        @click="handleSumbmit"
+        class="bg-blue-500 text-white px-4 py-2 uppercase rounded-md"
+      >
+        send
+      </button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const comments = ref([
+  {
+    id: 1,
+    author: "John Doe",
+    profileImg: "/img/user/angela.webp",
+    text: "This is my first comment Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, reprehenderit! Quam debitis laborum ipsa provident commodi dolor recusandae. Officia natus dolor delectus ullam iusto ducimus ipsam nemo placeat itaque facilis!",
+    createdAt: "1 month ago",
+    replies: [
+      // {
+      //   id: 2,
+      //   author: "Jane Doe",
+      //   profileImg: "/img/user/anna.webp",
+      //   text: "Replying to John's comment. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, quisquam!",
+      //   createdAt: "3 weeks ago",
+      // },
+      // More replies can be added here
+    ],
+  },
+]);
+
+const commentInput = ref("");
+
+const handleInput = () => {
+  commentInput.value = commentInput.innerText;
+};
+
+const handleSumbmit = () => {
+  if (!commentInput.value) {
+    return;
+  }
+  const commentObj = {
+    id: Math.floor(Math.random() * 1000),
+    author: "John Doe",
+    profileImg: "",
+    text: commentInput.value,
+    createdAt: new Date(),
+    replies: [], // Initialize replies for the new comment
+  };
+  comments.value.push(commentObj);
+
+  // Reset
+  commentInput.value = "";
+};
+
+const editComment = () => {};
+
+const deleteComment = () => {};
+</script>
+
+<style lang="scss" scoped></style>
