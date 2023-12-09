b008c8a9b2884821bd4678c122b44583037a1f43
Bishesh <bishesh.sunam@gmail.com>
2023-12-09 20:56:54 +1300

    feat: add layout for job listing component

---
+++ components/JobCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative">
+    <div class="absolute z-0 inset-0 -ml-2 bg-red-400 rounded-xl"></div>
+    <div class="p-2 bg-white text-gray-900 rounded-md relative z-10">
+      <div class="h-20 w-20 rounded-full">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <p>company name</p>
+          <p>New!</p>
+          <p>Feautured</p>
+        </div>
+        <h2>Job Title</h2>
+        <div>
+          <p>1d ago</p>
+          <p>Part Time</p>
+          <p>Remote</p>
+        </div>
+      </div>
+      <div v-for="(item, index) in 4" :key="index">
+        <p>{{ item }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/job-listing.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-40 bg-gray-600"></div>
+  <div class="p-6 flex flex-col gap-12">
+    <div v-for="(item, index) in 5" :key="index">
+      <JobCard />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
